03b97b07340060ccaefae9f8050cf6abaabd19c8
dannopek <daniel.pekarcik@gmail.com>
2020-10-19 21:35:23 +0200

    bug fix

--- mapper.py
+++ mapper.py
@@ -3,7 +3,7 @@
 import sys
 import re
 
-apache_access_log_pattern = '^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S+)?\s*" (\d{3}) (\S+) "(\S+)" "(\S+)" (\S+) "(\S+)" "(\S+)"? (\S+)ms'
+apache_access_log_pattern = '^(\S+) - (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)\s*(\S+)?\s*" (\d{3}) (\S+) "(\S+)" "(\S+)" (\S+) "(\S+)" "(\S+)"? (\S+)ms'
 
 for line in sys.stdin:
     match = re.search(apache_access_log_pattern, line)
@@ -13,20 +13,19 @@ for line in sys.stdin:
 
     curLog = {
         'host': match.group(1),
-        'client_identd': match.group(2),
-        'user_id': match.group(3),
-        'date_time': match.group(4),
-        'method': match.group(5),
-        'request_path': match.group(6),
-        'protocol': match.group(7),
-        'response_code': match.group(8),
-        'content_size': match.group(9),
-        'request_referrer': match.group(10), # always -
-        'request_user_agent': match.group(11), # always -
-        'number_of_requests_received_since_started': match.group(12),
-        'router_name': match.group(13),
-        'server_url': match.group(14),
-        'request_duration': match.group(15),
+        'client_user_name_if_available': match.group(2),
+        'date_time': match.group(3),
+        'method': match.group(4),
+        'request_path': match.group(5),
+        'protocol': match.group(6),
+        'response_code': match.group(7),
+        'content_size': match.group(8),
+        'request_referrer': match.group(9), # always -
+        'request_user_agent': match.group(10), # always -
+        'number_of_requests_received_since_started': match.group(11),
+        'router_name': match.group(12),
+        'server_url': match.group(13),
+        'request_duration': match.group(14),
     }
 
     # print(line)
